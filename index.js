45d0e633fc1018b1f0f195e0c5b54bfac67cfcf6
yemelyn <pommy.shortcake@gmail.com>
2016-08-02 19:23:46 +0000

    revised after code review

--- index.js
+++ index.js
@@ -3,30 +3,36 @@ const url = 'http://substack.net/images/';
 const cheerio = require('cheerio');
 const fs = require('fs');
 const json2csv = require('json2csv');
-var fields = ['File Permission', 'Absolute URL', 'File Type'];
-var results = []
+var fields = [];
+var results = [];
 
+
+function createRowConverter($){
+  return function getRows (i, element){
+    var permission = $(this).find('td').first().text();
+    var relurl = $(this).find('td').last().find('a').attr('href');
+    var aburl = 'http://substack.net'+relurl;
+    var extension = relurl.split('.')[1]
+    var row = {
+      'File Permission': permission,
+      'Absolute URL': aburl,
+      'File Type': extension
+    }
+    results.push(row);
+    fields = Object.keys(row);
+  }
+}
 
 request(url, function (error, response, body) {
   if (!error && response.statusCode == 200) {
     const $ = cheerio.load(body);
-    $('tr').each(function(i, element){
-      var permission = $(this).find('td').first().text();
-      var relurl = $(this).find('td').last().find('a').attr('href');
-      var aburl = 'http://substack.net'+relurl;
-      var extension = relurl.split('.')[1]
-      var row = {
-        'File Permission': permission,
-        'Absolute URL': aburl,
-        'File Type': extension
-      }
-      results.push(row);
-    });
+    $('tr').each(createRowConverter($));
     var csv = json2csv({data: results, fields: fields});
     fs.writeFile('output.csv', csv, function(err) {
       if (err) throw err;
       console.log('It\'s saved!');
     })
+  } else {
+    return console.error('upload failed:', error);
   }
-
 });
