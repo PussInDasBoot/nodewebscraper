86764b33963d362c8eaefcd6146874643197e6d8
yemelyn <pommy.shortcake@gmail.com>
2016-08-02 19:02:16 +0000

    some refactoring

--- index.js
+++ index.js
@@ -10,15 +10,11 @@ var results = []
 request(url, function (error, response, body) {
   if (!error && response.statusCode == 200) {
     const $ = cheerio.load(body);
-    var permissionarray = [];
-    var urlarray = [];
-    var results = [];
     $('tr').each(function(i, element){
       var permission = $(this).find('td').first().text();
       var relurl = $(this).find('td').last().find('a').attr('href');
       var aburl = 'http://substack.net'+relurl;
       var extension = relurl.split('.')[1]
-      // console.log(permission);
       var row = {
         'File Permission': permission,
         'Absolute URL': aburl,
@@ -27,7 +23,6 @@ request(url, function (error, response, body) {
       results.push(row);
     });
     var csv = json2csv({data: results, fields: fields});
-    console.log(csv);
     fs.writeFile('output.csv', csv, function(err) {
       if (err) throw err;
       console.log('It\'s saved!');
