a9e99d1be629f591df6c45f1167fc14d99d48459
yemelyn <pommy.shortcake@gmail.com>
2016-08-02 18:31:55 +0000

    parsed using cheerio

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const request = require('request');
 const url = 'http://substack.net/images/';
+const cheerio = require('cheerio');
+
 
 request(url, function (error, response, body) {
   if (!error && response.statusCode == 200) {
-    // console.log(body);
+    const $ = cheerio.load(body);
+    $('tr').each(function(i, element){
+      var permission = $(this).find('td').first().text();
+      var relurl = $(this).find('td').last().find('a').attr('href');
+      var aburl = 'http://substack.net'+relurl;
+      var extension = relurl.split('.')[1]
+      console.log(permission);
+    });
   }
 
 });
